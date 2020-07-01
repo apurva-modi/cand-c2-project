4907852204353dc4120cd3df86f88f53282116eb
Apurva Modi <43246880+apurva-modi@users.noreply.github.com>
2020-07-01 01:59:11 -0700

    Updated Excercise_1 terraform main.tf

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,7 +1,29 @@
 # TODO: Designate a cloud provider, region, and credentials
-
+provider "aws" {
+  region = "us-east-1"
+  shared_credentials_file = "/Users/apurvamodi/.aws/credentials" 
+}
 
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
+resource "aws_instance" "UdacityT2" {
+    count = 4
+    ami = "ami-09d95fab7fff3776c"
+    instance_type = "t2.micro"
+    subnet_id = "subnet-a83cfcce"
+    
+    tags = {
+        Name = "Udacity T2"
+    }
 
+}
 
 # TODO: provision 2 m4.large EC2 instances named Udacity M4
+resource "aws_instance" "UdacityM4" {
+    ami = "ami-09d95fab7fff3776c"
+    instance_type = "m4.large"
+    subnet_id = "subnet-a83cfcce"
+    count = 2
+    tags  = {
+        Name = "Udacity M4"
+    }
+}
